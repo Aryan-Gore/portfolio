3d6c4a1ae49403e379c5b845523417c9e764c31e
Aryan Gore <aryangore130@gmail.com>
2026-01-25 17:54:41 +0530

    cv added

--- script2.js
+++ script2.js
@@ -160,8 +160,6 @@ function initContactForm() {
             }
         );
     });
-    // =================download cv=================
-    function downloadCV() {
-  window.location.href = "assets/Aryan_Gore_Resume.pdf";
-}
+
+      
 }
