4f66fd970a6d501f8308d90a4398e15caa8d2826
Aryan Gore <aryangore130@gmail.com>
2026-07-26 14:09:12 +0530

    loader

--- script2.js
+++ script2.js
@@ -1,3 +1,18 @@
+// ================================
+// LOADER
+// ================================
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    if (loader) {
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 1000);
+    }
+});
 // -------------------- DOM CONTENT LOADED --------------------
 document.addEventListener('DOMContentLoaded', function() {
 
